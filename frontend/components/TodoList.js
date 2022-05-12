c82abf10cea2d2343d7b3810287b375dc9db1024
Kohlten Bills <kohltenbillsy@gmail.com>
2022-05-12 17:55:58 -0600

    complete all the way through

--- frontend/components/TodoList.js
+++ frontend/components/TodoList.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Todo from './Todo'
 
 export default class TodoList extends React.Component {
   render() {
@@ -8,7 +9,13 @@ export default class TodoList extends React.Component {
           <div id='error'>Error: {this.props.error}</div>
           {
             this.props.todos.reduce((acc, todo) => {
-              if (this.props.displayCompleted || !todo.completed) return acc.concat(<div onClick={this.props.toggleCompleted(todo.id)} key={todo.id}> {todo.name} {todo.completed ? '✔' : ''}</div>)
+              if (this.props.displayCompleted || !todo.completed) return acc.concat(
+                <Todo
+                toggleCompleted={this.props.toggleCompleted}
+                todo={todo}
+                key={todo.id}
+                />
+              )
               return acc
             },[])
           }
