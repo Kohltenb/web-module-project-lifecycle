3288beb2264368620c47d9b7f30e8626033a9f5a
Kohlten Bills <kohltenbillsy@gmail.com>
2022-05-12 16:30:08 -0600

    testing this method of commiting

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,64 @@
 import React from 'react'
+import axios from 'axios'
 
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+
+  state = {
+    todos: [],
+    error: '',
+    todoNameInput: '',
+  }
+  onTodoNameChange = evt => {
+    const { value } = evt.target
+    this.setState({...this.state, todoNameInput: value})
+  }
+  postNewTodo = () => {
+    axios.post(URL, {name: this.state.todoNameInput})
+    .then(res => {
+     this.fetchAllTodos()
+     this.setState({...this.state, todoNameInput: ''})
+    }) 
+    .catch(err => {
+      this.setState({ ...this.state, error: err.response.data.message })
+    })
+  }
+  onTodoFormSubmit = evt => {
+    evt.preventDefault()
+    this.postNewTodo()
+  }
+  fetchAllTodos = () => {
+    axios.get(URL)
+      .then(res => {
+        this.setState({ ...this.state, todos: res.data.data })
+      })
+      .catch(err => {
+        this.setState({ ...this.state, error: err.response.data.message })
+      })
+  }
+  componentDidMount() {
+    this.fetchAllTodos()
+  }
+
   render() {
-    return null
+    return (
+      <div>
+        <div>
+          <h1> Todos: </h1>
+          <div id='error'>Error: {this.state.error}</div>
+          {
+            this.state.todos.map(todo => {
+              return <div key={todo.id}> {todo.name} </div>
+            })
+          }
+        </div>
+          <form id='todoForm' onSubmit={this.onTodoFormSubmit}>
+        <input value={this.state.todoNameInput} onChange={this.onTodoNameChange}type='text' placeholder='todo'/>
+        <input type='submit'></input>
+        <button>clear</button>
+        </form>
+      </div>
+    )
   }
 }
